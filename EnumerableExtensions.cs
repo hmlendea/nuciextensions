8f08cff6d0ce8ca408c3ce7c652e24d787a46ef2
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2019-03-12 16:33:40 +0200

    GetDuplicates IEnumerable extension

--- EnumerableExtensions.cs
+++ EnumerableExtensions.cs
@@ -30,5 +30,27 @@ namespace NuciExtensions
 
             return enumerable.ElementAt(random.Next(enumerable.Count()));
         }
+        
+        /// <summary>
+        /// Gets the duplicated elements.
+        /// </summary>
+        /// <param name="source">The collection.</param>
+        /// <returns>The duplicated elements.</returns>
+        public static IEnumerable<T> GetDuplicates<T>(this IEnumerable<T> source)
+        {
+            HashSet<T> itemsSeen = new HashSet<T>();
+            HashSet<T> itemsYielded = new HashSet<T>();
+
+            foreach (T item in source)
+            {
+                if (!itemsSeen.Add(item))
+                {
+                    if (itemsYielded.Add(item))
+                    {
+                        yield return item;
+                    }
+                }
+            }
+        }
     }
 }
