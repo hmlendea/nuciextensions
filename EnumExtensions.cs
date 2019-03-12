e06e12829ec798313e381eae89fa642c18f6c622
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2019-03-12 16:32:13 +0200

    Enum extension for getting the display name

---
+++ EnumExtensions.cs
@@ -0,0 +1,34 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace NuciExtensions
+{
+    /// <summary>
+    /// Enumeration extensions.
+    /// </summary>
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Gets the display name of the enumeration item.
+        /// </summary>
+        /// <returns>The display name string.</returns>
+        /// <param name="value">Enumeration item.</param>
+        public static string GetDisplayName(this Enum value)
+        {
+            DisplayAttribute displayAttribute = value
+                .GetType()
+                .GetMember(value.ToString())
+                .FirstOrDefault()
+                .GetCustomAttribute<DisplayAttribute>();
+
+            if (displayAttribute != null)
+            {
+                return displayAttribute.GetName();
+            }
+
+            return value.ToString();
+        }
+    }
+}
