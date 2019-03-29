c0f755c361ad3552227fa1a83d67310828890912
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2019-03-29 17:35:17 +0200

    Started writing UTs

---
+++ NuciExtensions.UnitTests/DictionaryExtensionsTests.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using NuciExtensions;
+
+namespace NuciExtensions.UnitTests
+{
+    public class DictionaryExtensionsTests
+    {
+        [Test]
+        public void AddOrUpdate_KeyExists_ValueUpdated()
+        {
+            string testKey = "testKeyTest";
+            string testValue = "new value to update";
+
+            Dictionary<string, string> dict = new Dictionary<string, string>
+            {
+                { testKey, "old value" }
+            };
+
+            dict.AddOrUpdate(testKey, testValue);
+            
+            AssertThatDictionaryPairExists(dict, testKey, testValue);
+        }
+
+        [Test]
+        public void AddOrUpdate_KeyDoesntExist_PairAdded()
+        {
+            string testKey = "testKeyTest";
+            string testValue = "new value to add";
+
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+
+            dict.AddOrUpdate(testKey, testValue);
+            
+            AssertThatDictionaryPairExists(dict, testKey, testValue);
+        }
+        
+        [Test]
+        public void TryGetValue_KeyDoesNotExist_NullReturned()
+        {
+            string testKey = "testKeyTest";
+
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+
+            string actual = dict.TryGetValue(testKey);
+            
+            Assert.AreEqual(null, actual);
+        }
+
+        void AssertThatDictionaryPairExists<TKey, TValue>(Dictionary<TKey, TValue> dict, TKey key, TValue value)
+        {
+            Assert.IsTrue(dict.ContainsKey(key));
+            Assert.AreEqual(value, dict[key]);
+        }
+    }
+}
