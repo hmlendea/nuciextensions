71b828b80a557dec82d0cb8e5a54450a5b01cfa6
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2019-09-10 12:27:15 +0300

    ReplaceFirst unit tests

--- NuciExtensions.UnitTests/StringExtensionsTests.cs
+++ NuciExtensions.UnitTests/StringExtensionsTests.cs
@@ -1,9 +1,85 @@
+using System;
+
 using NUnit.Framework;
 
 namespace NuciExtensions.UnitTests
 {
     public class StringExtensionsTests
     {
+        [Test]
+        public void ReplaceFirst_SourceIsNull_ThrowsNullReferenceException()
+        {
+            string source = null;
+            string oldValue = "test";
+            string newValue = "this";
+
+            Assert.Throws<NullReferenceException>(() => source.ReplaceFirst(oldValue, newValue));
+        }
+
+        [Test]
+        public void ReplaceFirst_SourceIsEmpty_ReturnsEmpty()
+        {
+            string source = string.Empty;
+            string oldValue = "test";
+            string newValue = "this";
+            string expected = string.Empty;
+
+            Assert.AreEqual(expected, source.ReplaceFirst(oldValue, newValue));
+        }
+
+        [Test]
+        public void ReplaceFirst_OldValueIsNull_ThrowsArgumentNullException()
+        {
+            string source = "test string test text";
+            string oldValue = null;
+            string newValue = "this";
+
+            Assert.Throws<ArgumentNullException>(() => source.ReplaceFirst(oldValue, newValue));
+        }
+
+        [Test]
+        public void ReplaceFirst_OldValueIsEmpty_ThrowsArgumentException()
+        {
+            string source = "test string test text";
+            string oldValue = string.Empty;
+            string newValue = "this";
+
+            Assert.Throws<ArgumentException>(() => source.ReplaceFirst(oldValue, newValue));
+        }
+
+        [Test]
+        public void ReplaceFirst_NewValueIsNull_ReturnsCorrectString()
+        {
+            string source = "test string test text";
+            string oldValue = "test";
+            string newValue = null;
+            string expected = " string test text";
+
+            Assert.AreEqual(expected, source.ReplaceFirst(oldValue, newValue));
+        }
+
+        [Test]
+        public void ReplaceFirst_NewValueIsEmpty_ReturnsCorrectString()
+        {
+            string source = "test string test text";
+            string oldValue = "test";
+            string newValue = string.Empty;
+            string expected = " string test text";
+
+            Assert.AreEqual(expected, source.ReplaceFirst(oldValue, newValue));
+        }
+
+        [Test]
+        public void ReplaceFirst_ReturnsCorrectString()
+        {
+            string source = "test string test text";
+            string oldValue = "test";
+            string newValue = "this";
+            string expected = "this string test text";
+
+            Assert.AreEqual(expected, source.ReplaceFirst(oldValue, newValue));
+        }
+
         [Test]
         public void RemoveDiacritics_ReturnsCorrectValue()
         {
