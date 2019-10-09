efd38d25386c9c8b6ea05cb3635a348bb21f14d8
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2019-10-09 13:45:59 +0300

    List.Pop exception handling + UTs

--- NuciExtensions.UnitTests/ListExtensionsTests.cs
+++ NuciExtensions.UnitTests/ListExtensionsTests.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using NUnit.Framework;
-
-using NuciExtensions;
 
 namespace NuciExtensions.UnitTests
 {
@@ -43,6 +42,35 @@ namespace NuciExtensions.UnitTests
             IList<int> actual = collection.Shuffle();
             
             Assert.AreEqual(collection.Count, actual.Count);
+        }
+
+        [Test]
+        public void Pop_ListIsPopulated_LastElementIsRemoved()
+        {
+            IList<int> collection = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            IList<int> actualCollection = collection.ToList();
+            actualCollection.Pop();
+            
+            Assert.AreEqual(collection.Count - 1, actualCollection.Count);
+        }
+
+        [Test]
+        public void Pop_ListIsPopulated_LastElementIsReturned()
+        {
+            IList<int> collection = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            int actual = collection.ToList().Pop();
+            
+            Assert.AreEqual(collection.Last(), actual);
+        }
+
+        [Test]
+        public void Pop_ListIsEmpty_ThrowsIndexOutOfRangeException()
+        {
+            IList<int> collection = new List<int>();
+
+            Assert.Throws<IndexOutOfRangeException>(() => collection.Pop());
         }
     }
 }
