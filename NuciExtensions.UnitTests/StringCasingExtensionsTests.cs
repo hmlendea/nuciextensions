2d1da139d1cdfe6fa665a6d02ce024e61924d5d6
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2023-05-24 19:32:22 +0300

    More UTs

--- NuciExtensions.UnitTests/StringCasingExtensionsTests.cs
+++ NuciExtensions.UnitTests/StringCasingExtensionsTests.cs
@@ -17,5 +17,20 @@ namespace NuciExtensions.UnitTests
         [TestCase("the words that are Already Capitalised remain CAPITALISED", "The words that are Already Capitalised remain CAPITALISED")]
         public void GivenAString_WhenConvertingToSentanceCase_ThenEveryWordIsCapitalised(string text, string expectedResult)
             => Assert.That(text.ToSentanceCase(), Is.EqualTo(expectedResult));
+
+        [Test]
+        [TestCase("i want This to be in snake case", "i_want_This_to_be_in_snake_case")]
+        public void GivenAString_WhenConvertingToSnakeCase_ThenEveryWordIsSeparatedByUnderscore(string text, string expectedResult)
+            => Assert.That(text.ToSnakeCase(), Is.EqualTo(expectedResult));
+
+        [Test]
+        [TestCase("this is so GonNa be upper snake case", "THIS_IS_SO_GONNA_BE_UPPER_SNAKE_CASE")]
+        public void GivenAString_WhenConvertingToUpperSnakeCase_ThenEveryWordIsUppercaseAndSeparatedByUnderscore(string text, string expectedResult)
+            => Assert.That(text.ToUpperSnakeCase(), Is.EqualTo(expectedResult));
+
+        [Test]
+        [TestCase("lower SNAKE Case please", "lower_snake_case_please")]
+        public void GivenAString_WhenConvertingToLowerSnakeCase_ThenEveryWordIsLowercaseAndSeparatedByUnderscore(string text, string expectedResult)
+            => Assert.That(text.ToLowerSnakeCase(), Is.EqualTo(expectedResult));
     }
 }
